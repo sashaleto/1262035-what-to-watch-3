1fcbbf66af607a55ec82d70d5c9970f62f0a79f0
Marochkina <marochkina@gfcc.su>
2020-03-03 12:59:50 +0300

    Добавлен компонент карточки фильма

---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const getPosterName = (title) => {
+  return title.toLowerCase().replace(/[.,!:]/g, ``).split(` `).join(`-`);
+};
+
+const MovieCard = ({film, onFilmTitleClick}) => {
+  return (
+    <article className="small-movie-card catalog__movies-card">
+      <div className="small-movie-card__image">
+        <img src={`img/${getPosterName(film.title)}.jpg`}
+          alt={film.title} width="280" height="175"/>
+      </div>
+      <h3 className="small-movie-card__title" onClick={onFilmTitleClick}>
+        <a className="small-movie-card__link" href="movie-page.html">{film.title}</a>
+      </h3>
+    </article>
+  );
+};
+
+MovieCard.propTypes = {
+  film: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+  onFilmTitleClick: PropTypes.func.isRequired
+};
+
+export default MovieCard;
