65e95d24cac5639973bc6946bc9c378c54b04dbe
Marochkina <marochkina@gfcc.su>
2020-03-04 18:51:35 +0300

    Преобразование app в классовый компонент; подключение роутера

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Main from '../main/main.jsx';
+import MoviePage from '../movie-page/movie-page.jsx';
 
 const onFilmTitleClick = () => {};
 
-const App = ({films, movieData}) => {
-  return (
-    <Main films={films} movieData={movieData} onFilmTitleClick={onFilmTitleClick}/>
-  );
-};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {films, movieData} = this.props;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main films={ films } movieData={ movieData } onFilmTitleClick={ onFilmTitleClick }/>
+          </Route>
+          <Route exact path="/movie-page">
+            <MoviePage/>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   films: PropTypes.array.isRequired,
