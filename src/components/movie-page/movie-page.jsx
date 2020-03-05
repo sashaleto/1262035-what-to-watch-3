1ec98253bc67a149291b34f9845ff047551772a0
Marochkina <marochkina@gfcc.su>
2020-03-05 15:59:12 +0300

    Заполнение страницы о фильме данными из мока

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const MoviePage = () => {
+const MoviePage = ({movie}) => {
   return (
     <React.Fragment>
       <section className="movie-card movie-card--full">
         <div className="movie-card__hero">
           <div className="movie-card__bg">
-            <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel"/>
+            <img src={movie.poster.bg} alt={movie.title}/>
           </div>
 
           <h1 className="visually-hidden">WTW</h1>
@@ -30,10 +30,10 @@ const MoviePage = () => {
 
           <div className="movie-card__wrap">
             <div className="movie-card__desc">
-              <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+              <h2 className="movie-card__title">{movie.title}</h2>
               <p className="movie-card__meta">
-                <span className="movie-card__genre">Drama</span>
-                <span className="movie-card__year">2014</span>
+                <span className="movie-card__genre">{movie.genre}</span>
+                <span className="movie-card__year">{movie.releaseYear}</span>
               </p>
 
               <div className="movie-card__buttons">
@@ -58,7 +58,7 @@ const MoviePage = () => {
         <div className="movie-card__wrap movie-card__translate-top">
           <div className="movie-card__info">
             <div className="movie-card__poster movie-card__poster--big">
-              <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster"
+              <img src={movie.poster.big} alt={`${movie.title} poster`}
                 width="218"
                 height="327"/>
             </div>
@@ -79,33 +79,19 @@ const MoviePage = () => {
               </nav>
 
               <div className="movie-rating">
-                <div className="movie-rating__score">8,9</div>
+                <div className="movie-rating__score">{movie.rating.score}</div>
                 <p className="movie-rating__meta">
-                  <span className="movie-rating__level">Very good</span>
-                  <span className="movie-rating__count">240 ratings</span>
+                  <span className="movie-rating__level">{movie.rating.level}</span>
+                  <span className="movie-rating__count">{movie.rating.count} ratings</span>
                 </p>
               </div>
 
               <div className="movie-card__text">
-                <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided
-                                    over by
-                                    concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's
-                                    friend and
-                                    protege.</p>
+                <p>{movie.description}</p>
 
-                <p>Gustave prides himself on providing first-class service to the hotel's guests,
-                                    including
-                                    satisfying the sexual needs of the many elderly women who stay there. When one of
-                                    Gustave's
-                                    lovers dies mysteriously, Gustave finds himself the recipient of a priceless
-                                    painting and the
-                                    chief suspect in her murder.</p>
+                <p className="movie-card__director"><strong>Director: {movie.director}</strong></p>
 
-                <p className="movie-card__director"><strong>Director: Wes Andreson</strong></p>
-
-                <p className="movie-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude
-                                    Law, Willem Dafoe
-                                    and other</strong></p>
+                <p className="movie-card__starring"><strong>Starring: {movie.starring.join(`, `)} and other</strong></p>
               </div>
             </div>
           </div>
@@ -177,7 +163,23 @@ const MoviePage = () => {
 };
 
 MoviePage.propTypes = {
-
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    releaseYear: PropTypes.number.isRequired,
+    poster: PropTypes.shape({
+      big: PropTypes.string.isRequired,
+      bg: PropTypes.string.isRequired,
+    }).isRequired,
+    rating: PropTypes.shape({
+      score: PropTypes.number.isRequired,
+      level: PropTypes.string.isRequired,
+      count: PropTypes.number.isRequired,
+    }).isRequired,
+    director: PropTypes.string.isRequired,
+    starring: PropTypes.arrayOf(PropTypes.string).isRequired,
+    description: PropTypes.string.isRequired
+  }).isRequired,
 };
 
 export default MoviePage;
