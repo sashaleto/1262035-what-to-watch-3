cc510f84fe56fa7aeb1aa600c1eb5f802018e645
sashaleto <aleks.nesc@gmail.com>
2020-03-08 22:14:45 +0300

    Обновление тестов из-за обновления моков и переименования обработчика

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -15,30 +15,70 @@ const movieData = {
 
 const films = [
   {
-    title: `The Gentlemen`,
+    title: `Gangs of new york`,
+    posterImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/poster/Gangs_of_New_York_Poster.jpg`,
+    previewImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/preview/gangs_of_new_york.jpg`,
+    backgroundImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/background/gangs_of_new_york.jpg`,
+    description: `In 1862, Amsterdam Vallon returns to the Five Points area of New York City seeking revenge against Bill the Butcher, his father's killer.`,
+    rating: {
+      score: 8.8,
+      count: 370881,
+    },
+    director: `Martin Scorsese`,
+    starring: [`Leonardo DiCaprio`, `Cameron Diaz`, `Daniel Day-Lewis`],
+    genre: `Crime`,
+    released: 2002,
+    id: 1,
   }, {
-    title: `Sonic the Hedgehog`,
+    title: `Seven Years in Tibet`,
+    posterImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/poster/Seven_Years_in_Tibet.jpg`,
+    previewImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/preview/seven-years-in-tibet.jpg`,
+    backgroundImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/background/Seven_Years_in_Tibet.jpg`,
+    description: `True story of Heinrich Harrer, an Austrian mountain climber who became friends with the Dalai Lama at the time of China's takeover of Tibet.`,
+    rating: {
+      score: 3.6,
+      count: 112612,
+    },
+    director: `Jean-Jacques Annaud`,
+    starring: [`Brad Pitt`, `David Thewlis`, `BD Wong`],
+    genre: `Adventure`,
+    released: 1997,
+    id: 2,
   }, {
-    title: `Guns Akimbo`,
-  }, {
-    title: `Bloodshot`,
-  }
+    title: `Orlando`,
+    posterImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/poster/Orlando.jpg`,
+    previewImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/preview/orlando.jpg`,
+    backgroundImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/background/Orlando.jpg`,
+    description: `Young nobleman Orlando is commanded by Queen Elizabeth I to stay forever young. Miraculously, he does just that. The film follows him as he moves through several centuries of British history, experiencing a variety of lives and relationships along the way, and even changing sex.`,
+    rating: {
+      score: 2.6,
+      count: 12292,
+    },
+    director: `Sally Potter`,
+    starring: [`Tilda Swinton`, `Billy Zane`, `Quentin Crisp`],
+    genre: `Drama`,
+    released: 1992,
+    id: 3,
+  },
 ];
 
 it(`Should movie card title be pressed`, () => {
-  const onFilmTitleClick = jest.fn();
+  const onMovieCardClick = jest.fn();
+  const preventDefault = {
+    preventDefault: jest.fn()
+  };
 
   const mainScreen = mount(
       <Main
         films={films}
         movieData={movieData}
-        onFilmTitleClick={onFilmTitleClick}
+        onMovieCardClick={onMovieCardClick}
       />
   );
 
   const movieCardTitles = mainScreen.find(`.small-movie-card__title`);
 
-  movieCardTitles.forEach((title) => title.props().onClick());
+  movieCardTitles.forEach((card) => card.simulate(`click`, preventDefault));
 
-  expect(onFilmTitleClick.mock.calls.length).toBe(films.length);
+  expect(onMovieCardClick.mock.calls.length).toBe(films.length);
 });
--- src/components/movie-card/movie-card.e2e.test.js
+++ src/components/movie-card/movie-card.e2e.test.js
@@ -8,17 +8,30 @@ Enzyme.configure({
 });
 
 const film = {
-  title: `The Gentlemen`,
+  title: `Gangs of new york`,
+  posterImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/poster/Gangs_of_New_York_Poster.jpg`,
+  previewImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/preview/gangs_of_new_york.jpg`,
+  backgroundImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/background/gangs_of_new_york.jpg`,
+  description: `In 1862, Amsterdam Vallon returns to the Five Points area of New York City seeking revenge against Bill the Butcher, his father's killer.`,
+  rating: {
+    score: 8.8,
+    count: 370881,
+  },
+  director: `Martin Scorsese`,
+  starring: [`Leonardo DiCaprio`, `Cameron Diaz`, `Daniel Day-Lewis`],
+  genre: `Crime`,
+  released: 2002,
+  id: 1,
 };
 
 it(`Simulates hover on MovieCard and passing the arguments to handler`, () => {
-  const filmTitleClickHandler = jest.fn();
+  const movieCardClickHandler = jest.fn();
   const movieCardHoverHandler = jest.fn();
 
   const card = shallow(
       <MovieCard
         film={film}
-        onFilmTitleClick={filmTitleClickHandler}
+        onMovieCardClick={() => movieCardClickHandler(film.id)}
         onMovieCardHover={() => movieCardHoverHandler(film)}
       />
   );
