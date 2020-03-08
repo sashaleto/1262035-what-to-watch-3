1587528ec78e4d842a6da2afb8e23514a0b37ec0
sashaleto <aleks.nesc@gmail.com>
2020-03-08 20:13:24 +0300

    Переключение с карточки на экран полной информации по фильму

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,20 +4,43 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Main from '../main/main.jsx';
 import MoviePage from '../movie-page/movie-page.jsx';
 
-const onFilmTitleClick = () => {};
-
 class App extends PureComponent {
   constructor(props) {
     super(props);
+
+    this.state = {
+      activeMovieId: null,
+    };
+
+    this._handlerMovieCardClick = this._handlerMovieCardClick.bind(this);
+  }
+
+  _handlerMovieCardClick(id) {
+    this.setState({
+      activeMovieId: id,
+    });
+  }
+
+  _renderMainScreen() {
+    const {films, movieData} = this.props;
+    const {activeMovieId} = this.state;
+
+    if (activeMovieId !== null) {
+      return (<MoviePage movie={films.find((movie) => movie.id === activeMovieId)}/>);
+    }
+
+    return (
+      <Main films={ films } movieData={ movieData } onFilmTitleClick={ this._handlerMovieCardClick }/>
+    );
   }
 
   render() {
-    const {films, movieData, movieInfo} = this.props;
+    const {films} = this.props;
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
-            <Main films={ films } movieData={ movieData } onFilmTitleClick={ onFilmTitleClick }/>
+            {this._renderMainScreen()}
           </Route>
           <Route exact path="/movie-page">
             <MoviePage movie={ films[0] }/>
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,12 +3,18 @@ import PropTypes from 'prop-types';
 
 const MovieCard = ({film, onFilmTitleClick, onMovieCardHover}) => {
   return (
-    <article onMouseEnter={() => onMovieCardHover(film)} className="small-movie-card catalog__movies-card">
+    <article
+      onMouseEnter={() => onMovieCardHover(film)}
+      onClick={(e) => {
+        e.preventDefault();
+        onFilmTitleClick(film.id);
+      }}
+      className="small-movie-card catalog__movies-card">
       <div className="small-movie-card__image">
         <img src={film.previewImage}
           alt={film.title} width="280" height="175"/>
       </div>
-      <h3 className="small-movie-card__title" onClick={() => onFilmTitleClick()}>
+      <h3 className="small-movie-card__title">
         <a className="small-movie-card__link" href="movie-page.html">{film.title}</a>
       </h3>
     </article>
