a411e30ee2c4c3e84a6909070e008f3be4ca277f
sashaleto <aleks.nesc@gmail.com>
2020-03-08 22:14:45 +0300

    Обновление тестов из-за обновления моков и переименования обработчика

--- src/components/movie-card/movie-card.test.js
+++ src/components/movie-card/movie-card.test.js
@@ -3,13 +3,26 @@ import renderer from "react-test-renderer";
 import MovieCard from './movie-card';
 
 const film = {
-  title: `The Great Gatsby`,
+  title: `Gangs of new york`,
+  posterImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/poster/Gangs_of_New_York_Poster.jpg`,
+  previewImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/preview/gangs_of_new_york.jpg`,
+  backgroundImage: `https://htmlacademy-react-3.appspot.com/wtw/static/film/background/gangs_of_new_york.jpg`,
+  description: `In 1862, Amsterdam Vallon returns to the Five Points area of New York City seeking revenge against Bill the Butcher, his father's killer.`,
+  rating: {
+    score: 8.8,
+    count: 370881,
+  },
+  director: `Martin Scorsese`,
+  starring: [`Leonardo DiCaprio`, `Cameron Diaz`, `Daniel Day-Lewis`],
+  genre: `Crime`,
+  released: 2002,
+  id: 1,
 };
 
 it(`Render MovieCard`, () => {
   const tree = renderer
         .create(<MovieCard
-          film={film} onFilmTitleClick={() => {}} onMovieCardHover={() => {}}
+          film={film} onMovieCardClick={() => {}} onMovieCardHover={() => {}}
         />)
         .toJSON();
 
