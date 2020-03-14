dfc6f9fe6803f670acc6e568f960b2e049363699
sashaleto <aleks.nesc@gmail.com>
2020-03-15 00:27:14 +0300

    Подключение компонента видеоплеер и управление показом тизера

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import VideoPlayer from "../video-player/video-player.jsx";
 
-const MovieCard = ({film, onMovieCardClick, onMovieCardHover}) => {
+const MovieCard = ({film, onMovieCardClick, onMovieCardHover, onMovieCardLeave, isHovered}) => {
   return (
     <article
       onMouseEnter={() => onMovieCardHover(film)}
+      onMouseLeave={() => onMovieCardLeave()}
       onClick={(e) => {
         e.preventDefault();
         onMovieCardClick(film.id);
       }}
       className="small-movie-card catalog__movies-card">
       <div className="small-movie-card__image">
-        <img src={film.previewImage}
-          alt={film.title} width="280" height="175"/>
+        {isHovered ?
+          <VideoPlayer src={film.trailerLink} posterURL={film.previewImage} isMuted={true} isAutoplay={true}/> :
+          <img src={film.previewImage} alt={film.title} width="280" height="175"/>
+        }
       </div>
       <h3 className="small-movie-card__title">
         <a className="small-movie-card__link" href="movie-page.html">{film.title}</a>
@@ -26,9 +30,12 @@ MovieCard.propTypes = {
     title: PropTypes.string.isRequired,
     previewImage: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
+    trailerLink: PropTypes.string.isRequired,
   }).isRequired,
   onMovieCardClick: PropTypes.func.isRequired,
   onMovieCardHover: PropTypes.func.isRequired,
+  onMovieCardLeave: PropTypes.func.isRequired,
+  isHovered: PropTypes.bool.isRequired,
 };
 
 export default MovieCard;
