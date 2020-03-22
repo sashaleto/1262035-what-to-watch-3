30f487ab0894dacada28cdd9b817223891c8d8a7
sashaleto <aleks.nesc@gmail.com>
2020-03-22 23:56:37 +0300

    Выделение компонента "табы" для страницы фильма, моки для комментариев

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {movieLevelMapper} from '../../utils';
+import Tabs from "../tabs/tabs.jsx";
 
 const MoviePage = ({movie}) => {
   return (
@@ -64,37 +64,7 @@ const MoviePage = ({movie}) => {
                 height="327"/>
             </div>
 
-            <div className="movie-card__desc">
-              <nav className="movie-nav movie-card__nav">
-                <ul className="movie-nav__list">
-                  <li className="movie-nav__item movie-nav__item--active">
-                    <a href="#" className="movie-nav__link">Overview</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Details</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Reviews</a>
-                  </li>
-                </ul>
-              </nav>
-
-              <div className="movie-rating">
-                <div className="movie-rating__score">{movie.rating.score}</div>
-                <p className="movie-rating__meta">
-                  <span className="movie-rating__level">{movieLevelMapper(movie.rating.score)}</span>
-                  <span className="movie-rating__count">{movie.rating.count} ratings</span>
-                </p>
-              </div>
-
-              <div className="movie-card__text">
-                <p>{movie.description}</p>
-
-                <p className="movie-card__director"><strong>Director: {movie.director}</strong></p>
-
-                <p className="movie-card__starring"><strong>Starring: {movie.starring.join(`, `)} and other</strong></p>
-              </div>
-            </div>
+            <Tabs movie={movie}/>
           </div>
         </div>
       </section>
---
+++ src/mocks/comments.js
@@ -0,0 +1,19 @@
+export const comments = [{
+  id: 1,
+  user: {
+    id: 18,
+    name: `Sophie`
+  },
+  rating: 9,
+  comment: `I love this movie. This film is a milestone in cinematography. Great Immersive camera-work. This film is an experience and i has already seen it 4 times and I only see more quality of the film.`,
+  date: `2020-03-05T17:15:47.441Z`
+}, {
+  id: 2,
+  user: {
+    id: 10,
+    name: `Max`
+  },
+  rating: 4.4,
+  comment: `A movie that will take you to another world full of emotions.`,
+  date: `2020-02-28T17:15:47.441Z`
+}];
