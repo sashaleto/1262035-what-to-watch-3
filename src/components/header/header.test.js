aaf113a0aeb0967a29b48f5c9a01c3a1275bceaa
sashaleto <aleks.nesc@gmail.com>
2020-04-06 17:35:14 +0300

    Добавление ссылок на страницу входа / списка избранных фильмов

--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,16 +1,21 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import {Router} from "react-router-dom";
 import Header from "./header.jsx";
 import {AuthorizationStatus} from "../../reducer/user/user";
+import history from "../../history.js";
 
 const userAvatarUrl = `img/avatar.jpg`;
 
 it(`Render Header component with user avatar`, () => {
   const tree = renderer
-    .create(<Header
-      userAvatarUrl={userAvatarUrl}
-      userAuthStatus={AuthorizationStatus.AUTH}
-    />)
+    .create(
+        <Router history={history}>
+          <Header
+            userAvatarUrl={userAvatarUrl}
+            userAuthStatus={AuthorizationStatus.AUTH}
+          />
+        </Router>)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
@@ -18,10 +23,13 @@ it(`Render Header component with user avatar`, () => {
 
 it(`Render Header component with sign in link`, () => {
   const tree = renderer
-    .create(<Header
-      userAvatarUrl={userAvatarUrl}
-      userAuthStatus={AuthorizationStatus.NO_AUTH}
-    />)
+    .create(
+        <Router history={history}>
+          <Header
+            userAvatarUrl={userAvatarUrl}
+            userAuthStatus={AuthorizationStatus.NO_AUTH}
+          />
+        </Router>)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
